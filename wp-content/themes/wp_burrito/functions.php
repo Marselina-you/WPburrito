f68d5792fc4e761ea8fd77222cf73b6b436f459e
Marselina-you <marselina88@list.ru>
2023-06-07 11:51:42 +0300

    WooCommerce

--- wp-content/themes/wp_burrito/functions.php
+++ wp-content/themes/wp_burrito/functions.php
@@ -1,3 +1,11 @@
 <?php
+ function blog_assets() {
+    wp_enqueue_style('vendor', get_template_directory_uri() .'/assets/css/vendor.css');
+    
+    wp_enqueue_style('style', get_template_directory_uri() .'/assets/css/style.css');
 
+    wp_enqueue_script('main', get_template_directory_uri() .'/assets/js/main.js', array(), '20151215', true);
+}
+add_action('wp_enqueue_scripts', 'blog_assets');
+add_theme_support('menus');
           
--- wp-content/themes/wp_burrito/header.php
+++ wp-content/themes/wp_burrito/header.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Буррито - Чили-Бар</title>
-   
+<!DOCTYPE html><html lang="ru" class="page">
+	<head>
+		<meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"><meta name="theme-color" content="#111111">
+		
+		<title>Burrito</title>
+ <?php
+    wp_head();
+    ?>
 </head>
-
-
 		
